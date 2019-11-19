4261fb02f775d83baabed97b29575db27cc323dc
Ismael <ismael.espi3@gmail.com>
2019-11-19 08:28:31 +0100

    Creacion variable d
    -Declaracion int d
    -Inicializacion d = 15
    -Mostrar d en pantalla

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -4,13 +4,16 @@ public class Ejemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a, b;
+		int a, b, d;
 
 		a = 5;
 		System.out.printf("La variable \"a\" vale: %d", a);
 
 		b = 10;
 		System.out.printf("La variable \"b\" vale: %d", b);
+
+		d = 15;
+		System.out.printf("La variable \"d\" vale: %d", d);
 	}
 
 }
